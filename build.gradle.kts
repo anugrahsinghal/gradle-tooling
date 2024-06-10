2271cb2c9821c3db42dd8841f74bd9311d0128a6
Anugrah Singhal <anugrahsinghal1@gmail.com>
2024-06-10 17:52:24 +0530

    add libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
 }
 
 dependencies {
+    implementation("org.gradle:gradle-tooling-api:7.3.3")
+    // The tooling API need an SLF4J implementation available at runtime, replace this with any other implementation
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.10")
+
     testImplementation(kotlin("test"))
 }
 
