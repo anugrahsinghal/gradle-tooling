1bb92804996c123e089042118336af186ed1a89e
Anugrah Singhal <anugrahsinghal1@gmail.com>
2024-06-15 14:10:58 +0530

    the custom plugin

---
+++ src/main/java/CustomModelInjectionPlugin.java
@@ -0,0 +1,26 @@
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry;
+
+import javax.inject.Inject;
+
+/**
+ * A plugin that exposes a custom tooling model.
+ */
+public class CustomModelInjectionPlugin implements Plugin<Project> {
+    private final ToolingModelBuilderRegistry registry;
+
+    /**
+     * Need to use a {@link ToolingModelBuilderRegistry} to register the custom tooling model, so inject this into
+     * the constructor.
+     */
+    @Inject
+    public CustomModelInjectionPlugin(ToolingModelBuilderRegistry registry) {
+        this.registry = registry;
+    }
+
+    public void apply(Project project) {
+        // Register a builder for the custom tooling model
+        registry.register(new ConfigurationDependenciesModelBuilder());
+    }
+}
